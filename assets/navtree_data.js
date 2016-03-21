ef5ff4bd65b0a759ddf31042791eed8b807aec6a
viperwarp <michael@carnivallabs.com>
2016-03-22 10:16:44 +1300

    javadoc

---
+++ assets/navtree_data.js
@@ -0,0 +1,9 @@
+var NAVTREE_DATA =
+[ [ "com.carnival.sdk", "com/carnival/sdk/package-summary.html", [ [ "Interfaces", null, [ [ "Carnival.AttributesHandler", "com/carnival/sdk/Carnival.AttributesHandler.html", null, "" ], [ "Carnival.CarnivalHandler", "com/carnival/sdk/Carnival.CarnivalHandler.html", null, "" ], [ "Carnival.MessageDeletedHandler", "com/carnival/sdk/Carnival.MessageDeletedHandler.html", null, "" ], [ "Carnival.MessagesHandler", "com/carnival/sdk/Carnival.MessagesHandler.html", null, "" ], [ "Carnival.MessagesReadHandler", "com/carnival/sdk/Carnival.MessagesReadHandler.html", null, "" ], [ "Carnival.OnInAppNotificationDisplayListener", "com/carnival/sdk/Carnival.OnInAppNotificationDisplayListener.html", null, "" ], [ "Carnival.TagsHandler", "com/carnival/sdk/Carnival.TagsHandler.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "Carnival", "com/carnival/sdk/Carnival.html", null, "" ], [ "CarnivalMessageListener", "com/carnival/sdk/CarnivalMessageListener.html", null, "" ], [ "Message", "com/carnival/sdk/Message.html", null, "" ], [ "MessageActivity", "com/carnival/sdk/MessageActivity.html", null, "" ] ]
+, "" ], [ "Enums", null, [ [ "CarnivalImpressionType", "com/carnival/sdk/CarnivalImpressionType.html", null, "" ], [ "EventSource", "com/carnival/sdk/EventSource.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
